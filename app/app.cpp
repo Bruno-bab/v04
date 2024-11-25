a0b053ed9ce752db038214067fe0887191d1d76c
Bruno B. <brbaburi@vsite.hr>
2024-11-25 20:41:54 +0100

    v04 popravljen

--- app/app.cpp
+++ app/app.cpp
@@ -16,9 +16,9 @@ namespace vsite::oop::v4
         case 1:
             return std::make_unique<cockroach>();
         case 2:
-            return std::make_unique<tarantula>();
-        case 3:
             return std::make_unique<sparrow>();
+        case 3:
+            return std::make_unique<tarantula>();
         default:
             return nullptr;
         }
